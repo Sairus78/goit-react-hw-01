c96907b16f2f8b3a184c5d732a487bd08d49204e
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-10-21 23:36:42 +0300

    3

--- count.js
+++ count.js
@@ -1,23 +1,68 @@
 // import { writeFile } from "fs/promises"; // Імпорт fs з використанням ESM
 
-// const userData = {
-//   username: "Jacques Gluke",
-//   tag: "jgluke",
-//   location: "Ocho Rios, Jamaica",
-//   avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-//   stats: {
-//     followers: 5603,
-//     views: 4827,
-//     likes: 1308,
+// const transactions = [
+//   {
+//     id: "1e0700a2-5183-4291-85cc-2065a036a683",
+//     type: "invoice",
+//     amount: "964.82",
+//     currency: "LRD",
 //   },
-// };
+//   {
+//     id: "a30f821b-4d25-4ff0-abdd-e340b3f0dd2b",
+//     type: "payment",
+//     amount: "686.50",
+//     currency: "WST",
+//   },
+//   {
+//     id: "44dca67a-8e5a-4798-bf8e-b15efd4e1abd",
+//     type: "invoice",
+//     amount: "828.62",
+//     currency: "UGX",
+//   },
+//   {
+//     id: "ea8ed3dc-2b68-4a53-905a-53ecb0adef33",
+//     type: "withdrawal",
+//     amount: "527.80",
+//     currency: "ALL",
+//   },
+//   {
+//     id: "63ca02f9-d637-46b5-9237-f3b24425e029",
+//     type: "payment",
+//     amount: "862.44",
+//     currency: "AUD",
+//   },
+//   {
+//     id: "ed0263e8-59a5-4bf1-87e0-2bb88e53dc34",
+//     type: "withdrawal",
+//     amount: "907.00",
+//     currency: "GEL",
+//   },
+//   {
+//     id: "4eaab41b-b967-40ac-82ed-85fc66f646f1",
+//     type: "deposit",
+//     amount: "103.10",
+//     currency: "BWP",
+//   },
+//   {
+//     id: "9648a350-8469-42d5-8bf3-18090de5fe67",
+//     type: "withdrawal",
+//     amount: "322.32",
+//     currency: "MRO",
+//   },
+//   {
+//     id: "9c5c25fb-1a95-4b2f-8d1f-4c4426d677cc",
+//     type: "invoice",
+//     amount: "14.79",
+//     currency: "PYG",
+//   },
+// ];
 
-// const jsonData = JSON.stringify(userData, null, 2);
+// const jsonData = JSON.stringify(transactions, null, 2);
 
 // // Використовуємо writeFile з fs/promises для запису JSON у файл
 // try {
-//   await writeFile("userData.json", jsonData);
-//   console.log("Дані успішно записано у userData.json");
+//   await writeFile("transactions.json", jsonData);
+//   console.log("Дані успішно записано у transactions.json");
 // } catch (err) {
 //   console.error("Помилка запису у файл:", err);
 // }
