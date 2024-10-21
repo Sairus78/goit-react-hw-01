6211ae1637a7aac05247fbcd8321594eff83a588
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-10-21 23:31:31 +0300

    2

---
+++ count.js
@@ -0,0 +1,23 @@
+// import { writeFile } from "fs/promises"; // Імпорт fs з використанням ESM
+
+// const userData = {
+//   username: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: "Ocho Rios, Jamaica",
+//   avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
+
+// const jsonData = JSON.stringify(userData, null, 2);
+
+// // Використовуємо writeFile з fs/promises для запису JSON у файл
+// try {
+//   await writeFile("userData.json", jsonData);
+//   console.log("Дані успішно записано у userData.json");
+// } catch (err) {
+//   console.error("Помилка запису у файл:", err);
+// }
